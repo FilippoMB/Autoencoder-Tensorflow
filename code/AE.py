c117db9b538bba7e4b38f47ed49b839510a325d7
Filippo Bianchi <filippo.m.bianchi@uit.no>
2018-05-23 16:41:39 +0200

    removed object AE. train requires always the graph only. test always load and does not take graphs

--- code/AE.py
+++ code/AE.py
@@ -86,203 +86,181 @@ def next_batch(X, batch_size=1, shuffle=True):
         yield X_batch
 
 
-class customAE():
-
-    def make_graph(self,
-                   ae_layout=[2,1,2], # must be list of odd length (usually symmetric). First and last numbers are the input size. The central is code size.
-                   lin_dec=True,
-                   nonlinearity='relu',
-                   init='he',
-                   learning_rate=0.001,
-                   w_l2=0.001,
-                   max_gradient_norm=1.0,
-                   seed=None,
-                   model_id=None,
-                   ):
+# build computational graph
+def make_AEgraph(ae_layout=[2,1,2], # must be list of odd length (usually symmetric). First and last numbers are the input size. The central is code size.
+               lin_dec=True,
+               nonlinearity='relu',
+               init='he',
+               learning_rate=0.001,
+               w_l2=0.001,
+               max_gradient_norm=1.0,
+               seed=None):
+    
+    # initialize computational graph
+    g = tf.Graph()
+    with g.as_default():
         
-        # initialize computational graph
-        g = tf.Graph()
-        with g.as_default():
-            
-            tf.set_random_seed(seed) # only affects default graph
-        
-            # placeholders
-            self.encoder_inputs = tf.placeholder(shape=(None,ae_layout[0]), dtype=tf.float32, name='encoder_inputs')
-            self.keep_prob = tf.placeholder(dtype=tf.float32, name='keep_prob')
-                    
-            if len(ae_layout)%2 == 0:
-                raise RuntimeError('Bad AE layout!')
-            enc_size = len(ae_layout)//2
-            enc_layout = ae_layout[:enc_size+1]
-            dec_layout = ae_layout[enc_size:]
+        tf.set_random_seed(seed) # only affects default graph
     
-            # encoder
-            with tf.variable_scope('encoder'):
-                self.codes = build_network(self.encoder_inputs,
-                                           enc_layout,
-                                           self.keep_prob,
-                                           nonlinearity,
-                                           init,
-                                           w_l2)
-    
-    
-            # decoder
-            if lin_dec:
-                dec_nonlin = 'linear'
-            else:
-                dec_nonlin = nonlinearity
-            with tf.variable_scope('decoder'):
-                self.dec_out = build_network(self.codes,
-                                             dec_layout,
-                                             self.keep_prob,
-                                             dec_nonlin,
-                                             init,
-                                             w_l2)
-    
-           
-            # reconstruction loss
-            self.reconstruct_loss = tf.losses.mean_squared_error(labels=self.dec_out, predictions=self.encoder_inputs)
-    
-            # L2 loss
-            self.reg_loss = tf.reduce_sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
-    
-            # tot loss
-            self.tot_loss = self.reconstruct_loss + self.reg_loss 
-    
-            # Calculate and clip gradients
-            parameters = tf.trainable_variables()
-            optimizer = tf.train.AdamOptimizer(learning_rate)
-            gradients = tf.gradients(self.tot_loss, parameters)
-            clipped_gradients, _ = tf.clip_by_global_norm(gradients, max_gradient_norm)
-            self.update_step = optimizer.apply_gradients(zip(clipped_gradients, parameters))
-    
-            # save the graph        
-            if model_id is not None:  
-                print('export graph')
-                # put in a collection tensors and operations to call later 
-                g.add_to_collection('AE_collection', self.encoder_inputs) #0
-                g.add_to_collection('AE_collection', self.keep_prob) #1
-                g.add_to_collection('AE_collection', self.codes) #2
-                g.add_to_collection('AE_collection', self.dec_out) #3
-                g.add_to_collection('AE_collection', self.update_step) #4
-                g.add_to_collection('AE_collection', self.reconstruct_loss) #5
-                g.add_to_collection('AE_collection', self.reg_loss) #6 
-                g.add_to_collection('AE_collection', self.tot_loss) #7
+        # placeholders
+        encoder_inputs = tf.placeholder(shape=(None,ae_layout[0]), dtype=tf.float32, name='encoder_inputs')
+        keep_prob = tf.placeholder(dtype=tf.float32, name='keep_prob')
                 
-                saver = tf.train.Saver()                                    
-                saver.export_meta_graph('../models/AE_model_'+model_id+'.meta')
-                            
-        print('graph building is done')
-        
-        return g
+        if len(ae_layout)%2 == 0:
+            raise RuntimeError('Bad AE layout!')
+        enc_size = len(ae_layout)//2
+        enc_layout = ae_layout[:enc_size+1]
+        dec_layout = ae_layout[enc_size:]
+
+        # encoder
+        with tf.variable_scope('encoder'):
+            codes = build_network(encoder_inputs,
+                                       enc_layout,
+                                       keep_prob,
+                                       nonlinearity,
+                                       init,
+                                       w_l2)
 
 
-    def fit(self, 
-            X, 
-            Xval, 
-            batch_size=25, 
-            num_epochs=1000, 
-            dropout_prob=0.0,
-            model_id=None,
-            input_graph=None):
-        
-        if input_graph is None:
-            raise RuntimeError('computational graph not provided')
-                
-        with tf.Session(graph=input_graph) as sess:
-                                     
-            # restore a meta-graph 
-            if model_id is not None:       
-                print('restoring graph')    
-                model_name = '../models/AE_model_'+model_id
-                saver = tf.train.import_meta_graph(model_name+'.meta', clear_devices=True)                
-                self.encoder_inputs = input_graph.get_collection('AE_collection')[0]
-                self.keep_prob = input_graph.get_collection('AE_collection')[1]
-                self.update_step = input_graph.get_collection('AE_collection')[4]
-                self.reconstruct_loss = input_graph.get_collection('AE_collection')[5]
-                self.reg_loss = input_graph.get_collection('AE_collection')[6]
-                self.tot_loss = input_graph.get_collection('AE_collection')[7]
-                
-            else:
-                model_name = '../models/AE_model_default'
-                saver = tf.train.Saver()          
-                                    
-            # initialize trainable variables 
-            print('init vars')
-            sess.run(tf.global_variables_initializer())
+        # decoder
+        if lin_dec:
+            dec_nonlin = 'linear'
+        else:
+            dec_nonlin = nonlinearity
+        with tf.variable_scope('decoder'):
+            dec_out = build_network(codes,
+                                         dec_layout,
+                                         keep_prob,
+                                         dec_nonlin,
+                                         init,
+                                         w_l2)
 
-            # initialize training stuff
-            time_tr_start = time.time()
-            loss_track = []
-            min_val_loss = np.infty
-            
-            print('training start')            
-            try:
-                for t in range(num_epochs):
-                    for X_batch in next_batch(X, batch_size, True):
-                        fdtr = {self.encoder_inputs: X_batch,
-                                self.keep_prob: 1.0 - dropout_prob}
+       
+        # reconstruction loss
+        reconstruct_loss = tf.losses.mean_squared_error(labels=dec_out, predictions=encoder_inputs)
 
-                        _, train_loss = sess.run([self.update_step, self.tot_loss], fdtr)                        
-                        loss_track.append(train_loss)
+        # L2 loss
+        reg_loss = tf.reduce_sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
 
-                    # check training progress on the validation set
-                    if t % 100 == 0:
-                        fdvs = {self.encoder_inputs: Xval, 
-                                self.keep_prob: 1.0}
+        # tot loss
+        tot_loss = reconstruct_loss + reg_loss 
+
+        # Calculate and clip gradients
+        parameters = tf.trainable_variables()
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        gradients = tf.gradients(tot_loss, parameters)
+        clipped_gradients, _ = tf.clip_by_global_norm(gradients, max_gradient_norm)
+        update_step = optimizer.apply_gradients(zip(clipped_gradients, parameters))
+
+        # put in a collection placeholders and operations to call later 
+        g.add_to_collection('AE_collection', encoder_inputs) #0
+        g.add_to_collection('AE_collection', keep_prob) #1
+        g.add_to_collection('AE_collection', codes) #2
+        g.add_to_collection('AE_collection', dec_out) #3
+        g.add_to_collection('AE_collection', update_step) #4
+        g.add_to_collection('AE_collection', reconstruct_loss) #5
+        g.add_to_collection('AE_collection', reg_loss) #6 
+        g.add_to_collection('AE_collection', tot_loss) #7
                         
-                        (val_tot_loss,
-                         val_rec_loss,
-                         val_reg_loss) = sess.run([  self.tot_loss,
-                                                 self.reconstruct_loss,
-                                                 self.reg_loss], fdvs)
+    print('graph building is done')
+    
+    return g
 
-                        print("totL: %.3f, recL: %.3f, reg_loss: %.3f"%(val_tot_loss, val_rec_loss, val_reg_loss))
 
-                        # Save model yielding the lowest loss on validation
-                        if val_tot_loss < min_val_loss:
-                            min_val_loss = val_tot_loss
-                            saver.save(sess, model_name)
-
-            except KeyboardInterrupt:
-                print('training interrupted')
-
-        ttime = (time.time()-time_tr_start)/60
-        print("tot time:{}".format(ttime))
+def trainAE(X, 
+        Xval, 
+        batch_size=25, 
+        num_epochs=1000, 
+        dropout_prob=0.0,
+        save_id='default',
+        input_graph=None):
+    
+    if input_graph is None:
+        raise RuntimeError('an input_graph must be provided')
         
-        return loss_track
-
-
-    def transform(self, 
-                  Xte, 
-                  model_id=None,
-                  input_graph=None):
-
-        if input_graph is None:
-            raise RuntimeError('computational graph not provided')
-                
-        with tf.Session(graph=input_graph) as sess:
+#        input_graph.as_default()
             
-            # restore a meta-graph 
-            if model_id is not None:  
-                print('restoring graph')               
-                model_name = '../models/AE_model_'+model_id
-                saver = tf.train.import_meta_graph(model_name+'.meta', clear_devices=True)                
-                self.encoder_inputs = tf.get_collection('AE_collection')[0]
-                self.keep_prob = tf.get_collection('AE_collection')[1]
-                self.codes = tf.get_collection('AE_collection')[2]
-                self.dec_out = tf.get_collection('AE_collection')[3]
-                self.reconstruct_loss = tf.get_collection('AE_collection')[5]
-                
-            else:
-                model_name = '../models/AE_model_default'
-                saver = tf.train.Saver()
-                
-            print('restoring weights')                    
-            saver.restore(sess, model_name)
-                
-            # evaluate model on the test set
-            fdte = {self.encoder_inputs: Xte, self.keep_prob: 1.0}
-            te_rec_loss, te_code, te_reconstr = sess.run([self.reconstruct_loss, self.codes, self.dec_out], fdte)        
+    with tf.Session(graph=input_graph) as sess:
+                                 
+        # restore placeholders and operations                            
+        encoder_inputs = input_graph.get_collection('AE_collection')[0]
+        keep_prob = input_graph.get_collection('AE_collection')[1]
+        update_step = input_graph.get_collection('AE_collection')[4]
+        reconstruct_loss = input_graph.get_collection('AE_collection')[5]
+        reg_loss = input_graph.get_collection('AE_collection')[6]
+        tot_loss = input_graph.get_collection('AE_collection')[7]
         
-        return te_rec_loss, te_code, te_reconstr
+            
+        # initialize trainable variables 
+        print('init vars')
+        sess.run(tf.global_variables_initializer())
+
+        # initialize training stuff
+        time_tr_start = time.time()
+        loss_track = []
+        min_val_loss = np.infty
+        saver = tf.train.Saver() 
+        model_name = '../models/AE_model_'+save_id
+        
+        print('training start')            
+        try:
+            for t in range(num_epochs):
+                for X_batch in next_batch(X, batch_size, True):
+                    fdtr = {encoder_inputs: X_batch,
+                            keep_prob: 1.0 - dropout_prob}
+
+                    _, train_loss = sess.run([update_step, tot_loss], fdtr)                        
+                    loss_track.append(train_loss)
+
+                # check training progress on the validation set
+                if t % 100 == 0:
+                    fdvs = {encoder_inputs: Xval, 
+                            keep_prob: 1.0}
+                    
+                    (val_tot_loss,
+                     val_rec_loss,
+                     val_reg_loss) = sess.run([tot_loss,
+                                               reconstruct_loss,
+                                               reg_loss], fdvs)
+    
+                    print("totL: %.3f, recL: %.3f, reg_loss: %.3f"%(val_tot_loss, val_rec_loss, val_reg_loss))
+
+                    # Save model yielding the lowest loss on validation
+                    if val_tot_loss < min_val_loss:
+                        min_val_loss = val_tot_loss
+                        saver.save(sess, model_name)
+
+        except KeyboardInterrupt:
+            print('training interrupted')
+
+    ttime = (time.time()-time_tr_start)/60
+    print("tot time:{}".format(ttime))
+    
+    return loss_track
+
+
+def testAE(Xte, save_id=None):
+            
+    tf.reset_default_graph()
+    with tf.Session() as sess:
+        
+        model_name = '../models/AE_model_'+save_id
+        saver = tf.train.import_meta_graph(model_name+'.meta', clear_devices=True) 
+                
+        # restore op                 
+        encoder_inputs = tf.get_collection('AE_collection')[0]
+        keep_prob = tf.get_collection('AE_collection')[1]
+        codes = tf.get_collection('AE_collection')[2]
+        dec_out = tf.get_collection('AE_collection')[3]
+        reconstruct_loss = tf.get_collection('AE_collection')[5]
+            
+        # restore weights
+        saver.restore(sess, model_name)
+                    
+        # evaluate model on the test set
+        fdte = {encoder_inputs: Xte, keep_prob: 1.0}
+        te_rec_loss, te_code, te_reconstr = sess.run([reconstruct_loss, codes, dec_out], fdte)        
+        
+        print('reconstruct_loss = %.3f'%te_rec_loss)
+    
+    return te_rec_loss, te_code, te_reconstr
--- code/main.py
+++ code/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from AE import customAE
+from AE import make_AEgraph, trainAE, testAE
 import matplotlib.pyplot as plt
 from sklearn import datasets, preprocessing, model_selection
 
@@ -16,86 +16,55 @@ input_size = X.shape[1]
 Xtr, Xte, Ytr, Yte = model_selection.train_test_split(X, Y, test_size=0.2, stratify=Y)
 Xval = Xtr
 
-# create AE object
-AE = customAE()
+# build first AE graph
+g1 = make_AEgraph(ae_layout=[input_size,5,2,5,input_size],
+                         lin_dec=False,
+                         nonlinearity='maxout', # {relu, sigmoid, tanh, maxout}
+                         init='he', # {he, xav}
+                         learning_rate=0.01,
+                         w_l2=0.01,
+                         max_gradient_norm=1.0,
+                         seed=seed) 
 
-# ----- NO MODEL SAVING ------
-
-# build graph
-g = AE.make_graph(ae_layout=[input_size,5,2,5,input_size],
+# build second AE graph
+g2 = make_AEgraph(ae_layout=[input_size,5,2,5,input_size],
                          lin_dec=False,
                          nonlinearity='maxout', # {relu, sigmoid, tanh, maxout}
                          init='he', # {he, xav}
                          learning_rate=0.001,
                          w_l2=0.01,
                          max_gradient_norm=1.0,
-                         seed=seed,
-                         model_id=None) 
-
-# train AE
-AE.fit(Xtr,
-       Xval,
-       batch_size=25,
-       num_epochs=1000,
-       dropout_prob = 0.0,
-       model_id=None,
-       input_graph=g)
-
-# test AE
-te_rec_loss, te_code, te_reconstr = AE.transform(Xte, model_id=None, input_graph=g)
-plt.scatter(te_code[:,0], te_code[:,1], c=np.argmax(Yte,axis=1), cmap="tab10")
-plt.show()
-
-
-# ----- HANDLE MULTIPLE MODELS AT ONCE -----
-
-# build first AE
-g1 = AE.make_graph(ae_layout=[input_size,10,2,10,input_size],
-                             lin_dec=True,
-                             nonlinearity='relu', # {relu, sigmoid, tanh, maxout}
-                             init='xav', # {he, xav}
-                             learning_rate=0.001,
-                             w_l2=0.001,
-                             max_gradient_norm=1.0,
-                             seed=seed,
-                             model_id='id1') 
-
-# build second AE
-g2 = AE.make_graph(ae_layout=[input_size,2,input_size],
-                             lin_dec=False,
-                             nonlinearity='relu', # {relu, sigmoid, tanh, maxout}
-                             init='he', # {he, xav}
-                             learning_rate=0.001,
-                             w_l2=0.1,
-                             max_gradient_norm=1.0,
-                             seed=seed,
-                             model_id='id2') 
+                         seed=seed) 
 
 # train first AE
-AE.fit(Xtr,
+trainAE(Xtr,
        Xval,
        batch_size=25,
-       num_epochs=1000,
+       num_epochs=300,
        dropout_prob = 0.0,
-       model_id='id1',
+       save_id='id1',
        input_graph=g1)
 
-
-# train second AE
-AE.fit(Xtr,
-       Xval,
-       batch_size=25,
-       num_epochs=1000,
-       dropout_prob = 0.0,
-       model_id='id2',
-       input_graph=g2)
-    
 # test first AE
-te_rec_loss, te_code, te_reconstr = AE.transform(Xte, model_id='id1', input_graph=g1)
+te_rec_loss, te_code, te_reconstr = testAE(Xte, save_id='id1')
 plt.scatter(te_code[:,0], te_code[:,1], c=np.argmax(Yte,axis=1), cmap="tab10")
 plt.show()
 
+# train second AE
+trainAE(Xtr,
+       Xval,
+       batch_size=25,
+       num_epochs=2000,
+       dropout_prob = 0.0,
+       save_id='id2',
+       input_graph=g2)
+
 # test second AE
-te_rec_loss, te_code, te_reconstr = AE.transform(Xte, model_id='id2',input_graph=g2)
-plt.scatter(te_code[:,0], te_code[:,1], c=np.argmax(Yte,axis=1), cmap="Set1")
+te_rec_loss, te_code, te_reconstr = testAE(Xte, save_id='id2')
+plt.scatter(te_code[:,0], te_code[:,1], c=np.argmax(Yte,axis=1), cmap="tab10")
+plt.show()
+
+# test a pretrained model
+te_rec_loss, te_code, te_reconstr = testAE(Xte, save_id='pretrained')
+plt.scatter(te_code[:,0], te_code[:,1], c=np.argmax(Yte,axis=1), cmap="tab10")
 plt.show()
